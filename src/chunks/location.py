1b0ce1a43bfb1d4bfe933c0caad6d80daf1774bc
Kais Dukes <dukeskais79@gmail.com>
2023-07-02 10:43:06 +0100

    Bugfix: update chunk boundaries for dependendcy graphs that end with elision.

--- src/chunks/location.py
+++ src/chunks/location.py
@@ -6,3 +6,9 @@ class Location:
     chapter_number: int
     verse_number: int
     token_number: int
+
+    def __str__(self):
+        parts = [str(self.chapter_number), str(self.verse_number)]
+        if self.token_number > 0:
+            parts.append(str(self.token_number))
+        return ':'.join(parts)
