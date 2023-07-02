28b6c4689aaa4ad9cfe3be8c404e05671e110a46
Kais Dukes <dukeskais79@gmail.com>
2023-07-02 14:22:41 +0100

    Simple baseline: use punctuation.

--- src/chunks/preprocessor.py
+++ src/chunks/preprocessor.py
@@ -5,3 +5,16 @@ def preprocess(df: DataFrame):
     df['verse_end'] = (
         (df.groupby(['chapter_number', 'verse_number']).token_number.transform(max) == df.token_number)
         .astype(int))
+
+    df['punctuation'] = df['translation'].apply(_punctuation)
+
+
+PUNCTUATION = [',', '.', '\'', '\"', '!', '?']
+
+
+def _punctuation(text: str) -> str:
+    n = len(text)
+    for i in range(n - 1, -1, -1):
+        if text[i] not in PUNCTUATION:
+            return text[i+1:] if i < n - 1 else None
+    return text
--- src/models/simple_chunker.py
+++ src/models/simple_chunker.py
@@ -9,7 +9,12 @@ class SimpleChunker:
         predictions = df.copy()
 
         predictions['chunk_end'] = df.apply(
-            lambda row: int(row['verse_end'] or row['pause_mark'] != 0),
+            lambda row: int(row['verse_end'] or row['pause_mark'] != 0 or self._punctuation_boundary(row['punctuation'])),
             axis=1)
 
+        print(df['punctuation'].unique())
+
         return predictions
+
+    def _punctuation_boundary(self, punctuation: str):
+        return punctuation in ['.', '!', '?']
